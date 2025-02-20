34c3ae758154923291444ed25c55dfdbe2edb268
AlexanderDolnikov <sasha.dolnikov@gmail.com>
2025-02-20 16:26:47 -0500

    minor changes. buttons and some interface elements now work better

---
+++ AutoserviceApp/AutoserviceApp/DataAccess/Repositories/OrderRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using AutoserviceApp.Models;
+
+namespace AutoserviceApp.DataAccess.Repositories
+{
+    public class OrderRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public OrderRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            var orders = new List<Order>();
+
+            using (var connection = _context.GetConnection())
+            {
+                connection.Open();
+
+                var command = new SqlCommand("SELECT * FROM Заказ", connection);
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    orders.Add(new Order
+                    {
+                        Код = (int)reader["Код"],
+                        ДатаНачала = (DateTime)reader["ДатаНачала"],
+                        ДатаОкончания = (DateTime)reader["ДатаОкончания"],
+                        КодКлиента = (int)reader["КодКлиента"],
+                        КодАвтомобиля = (int)reader["КодАвтомобиля"]
+                    });
+                }
+            }
+
+            return orders;
+        }
+    }
+}
--- AutoserviceApp/AutoserviceApp/Views/OrdersView.xaml.cs
+++ AutoserviceApp/AutoserviceApp/Views/OrdersView.xaml.cs
@@ -1,66 +1,125 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Controls;
-using AutoserviceApp.Models; // Если Order находится в Models
-
+using AutoserviceApp.DataAccess.Repositories;
+using AutoserviceApp.Models;
+using AutoserviceApp.DataAccess;
 
 namespace AutoserviceApp.Views
 {
     public partial class OrdersView : UserControl
     {
+        private readonly OrderRepository _orderRepository;
+        private readonly DatabaseContext _context;
+        private List<Order> _orders;
+
         public OrdersView()
         {
             InitializeComponent();
+            _context = new DatabaseContext();
+            _orderRepository = new OrderRepository(_context);
             LoadOrders();
         }
 
         private void LoadOrders()
         {
-            var clients = new Dictionary<int, string>
-            {
-                { 1, "Иванов И.И." },
-                { 2, "Петров П.П." }
-            };
+            _orders = _orderRepository.GetAllOrders();
+            OrdersListPanel.Children.Clear();
 
-            List<Order> orders = new List<Order>
+            foreach (var order in _orders)
             {
-                new Order { Код = 1, ДатаНачала = DateTime.Parse("2024-01-10"), ДатаОкончания = DateTime.Parse("2024-01-20"), КодКлиента = 1 },
-                new Order { Код = 2, ДатаНачала = DateTime.Parse("2024-02-05"), ДатаОкончания = DateTime.Parse("2024-02-12"), КодКлиента = 2 }
-            };
+                // Создаем Border для рамки
+                Border orderBorder = new Border
+                {
+                    BorderBrush = new SolidColorBrush(Colors.Gray),
+                    BorderThickness = new Thickness(1),
+                    Margin = new Thickness(0, 5, 0, 5),
+                    Padding = new Thickness(5),
+                    Background = new SolidColorBrush(Colors.White)
+                };
 
-            OrdersList.ItemsSource = orders.Select(o => new
-            {
-                o.Код,
-                o.ДатаНачала,
-                o.ДатаОкончания,
-                Клиент = clients.ContainsKey(o.КодКлиента) ? clients[o.КодКлиента] : "Неизвестный клиент"
-            }).ToList();
+                // Внутри Border создаем StackPanel
+                StackPanel orderPanel = new StackPanel
+                {
+                    Orientation = Orientation.Horizontal
+                };
+
+                // Дата начала
+                TextBlock startDate = new TextBlock
+                {
+                    Text = order.ДатаНачала.ToString("dd.MM.yyyy"),
+                    Width = 100,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    TextAlignment = TextAlignment.Center
+                };
+
+                // Разделитель
+                Border separator1 = new Border
+                {
+                    Width = 2,
+                    Background = new SolidColorBrush(Colors.Gray),
+                    Margin = new Thickness(5, 0, 5, 0)
+                };
+
+                // Дата окончания
+                TextBlock endDate = new TextBlock
+                {
+                    Text = order.ДатаОкончания.ToString("dd.MM.yyyy"),
+                    Width = 100,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    TextAlignment = TextAlignment.Center
+                };
+
+                // Разделитель
+                Border separator2 = new Border
+                {
+                    Width = 2,
+                    Background = new SolidColorBrush(Colors.Gray),
+                    Margin = new Thickness(5, 0, 5, 0)
+                };
+
+                // Код клиента
+                TextBlock clientCode = new TextBlock
+                {
+                    Text = $"Клиент ID: {order.КодКлиента}",
+                    Width = 120,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    TextAlignment = TextAlignment.Center
+                };
+
+                // Кнопка "Подробнее"
+                Button detailsButton = new Button
+                {
+                    Content = "Подробнее",
+                    Background = new SolidColorBrush(Color.FromRgb(170, 212, 19)),
+                    Foreground = new SolidColorBrush(Colors.Black),
+                    Padding = new Thickness(5),
+                    FontWeight = FontWeights.Bold
+                };
+                detailsButton.Click += (sender, e) => ShowOrderDetails(order);
+
+                // Добавляем элементы в StackPanel
+                orderPanel.Children.Add(startDate);
+                orderPanel.Children.Add(separator1);
+                orderPanel.Children.Add(endDate);
+                orderPanel.Children.Add(separator2);
+                orderPanel.Children.Add(clientCode);
+                orderPanel.Children.Add(detailsButton);
+
+                // Добавляем StackPanel внутрь Border
+                orderBorder.Child = orderPanel;
+
+                // Добавляем Border в главный контейнер
+                OrdersListPanel.Children.Add(orderBorder);
+            }
         }
 
-
-
-        private void ShowOrderDetails_Click(object sender, RoutedEventArgs e)
+        private void ShowOrderDetails(Order order)
         {
-            Button button = sender as Button;
-            int orderId = (int)button.Tag;
-
-            MessageBox.Show($"Показать детали заказа {orderId}");
+            MessageBox.Show($"Детали заказа {order.Код}\nКлиент ID: {order.КодКлиента}\nАвтомобиль ID: {order.КодАвтомобиля}",
+                            "Детали заказа", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
-
